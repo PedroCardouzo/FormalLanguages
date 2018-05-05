54043e09c2f6fb06f979bee0c32a90cf72df923b
Pedro Cardouzo <pedro.cardouzo@gmail.com>
2018-05-05 17:52:13 -0300

    changed internal grammar representation to sets, as it is a closer resemblance to the real world counterpart

--- Grammar.py
+++ Grammar.py
@@ -7,10 +7,10 @@ Rule = namedtuple('Rule', ['head', 'tail'])
 class Grammar:
 
     def __init__(self, empty_symbol='V'):
-        self.terminals = []
-        self.variables = []
+        self.terminals = set()
+        self.variables = set()
         self.initial = None
-        self.rules = []
+        self.rules = set()
         self.empty_symbol = empty_symbol  # 'V' is default for empty symbol
 
     def __str__(self):
@@ -19,33 +19,33 @@ class Grammar:
         # P = {
         #     rules
         # }
-        terminals_string = ', '.join(self.terminals) # ['X', 'Y', 'Z'] -> 'X, Y, Z'
-        variables_string = ', '.join(self.variables) # ['a', 'b', 'c'] -> 'a, b, c'
+        terminals_string = ', '.join(self.terminals)  # ['a', 'b', 'c'] -> 'a, b, c'
+        variables_string = ', '.join(self.variables)  # ['X', 'Y', 'Z'] -> 'X, Y, Z'
         rules_string = self.__str_rules__()
         return 'G = ({' + variables_string + '}, {' + terminals_string + '}, P, ' + self.initial + ')\nP = {\n' + rules_string + '}'
 
     def __str_rules__(self):
         str_buffer = ''
-        buffer = []
         for variable in self.variables:
             rules_for_variable = filter(lambda x: x.head == variable, self.rules)  # filter for only one variable
             rules_for_variable = map(lambda x: x.tail, rules_for_variable)  # extract only the rules
 
             rules_for_variable = [' '.join(single_rule) for single_rule in rules_for_variable]
 
-            str_buffer += '\t' + variable + ' -> ' + ' | '.join(rules_for_variable) + '\n'  # return the formatted rule generation
+            str_buffer += '\t' + variable + ' -> ' + ' | '.join(
+                rules_for_variable) + '\n'  # return the formatted rule generation
         return str_buffer
 
     def read_grammar_from_file(self, filepath):
-        buffer = []  # the 4 buffer slots
+        buffer = []  # buffer slots
         buffer_pos = (a for a in range(0, 4))  # there are 4 elements that define a grammar
-        with open(filepath, 'r') as file:
+        with open(filepath, 'r', encoding='UTF-8-sig') as file:  # this removes special UTF-8 encoding "ï»¿"
             for line in file:
                 if line[0] == '#':
                     i = next(buffer_pos)
                     buffer.append([])  # new buffer slot
                 else:
-                    line = cleanLine(line, ']')
+                    line = clean_line(line, ']')
                     if line != '':
                         buffer[i].append(line)
 
@@ -54,42 +54,45 @@ class Grammar:
         self.generate_initial(buffer[2])
         self.generate_rules(buffer[3])
 
-        self.sort_variables() # so we can print in a more organized way
+        # self.sort_grammar()
+
+    def sort_grammar(self):
+        self.sort_variables()  # so we can print in a more organized way
         self.terminals.sort()
 
-    def sort_variables(self):
+    def sort_variables(self):  # sort but let initial variable @ position 1 in variables list
         self.variables.remove(self.initial)
         self.variables.sort()
-        self.variables = [self.initial] + self.variables
+        self.variables = {self.initial} + self.variables
 
     def generate_terminals(self, buffer):
         for encoded_symbol in buffer:
-            self.terminals.append(extractSymbol(encoded_symbol))
+            self.terminals.add(extract_symbol(encoded_symbol))
 
     def generate_variables(self, buffer):
         for encoded_symbol in buffer:
-            self.variables.append(extractSymbol(encoded_symbol))
+            self.variables.add(extract_symbol(encoded_symbol))
 
     def generate_initial(self, encoded_symbol):
-        self.initial = extractSymbol(encoded_symbol[0])
+        self.initial = extract_symbol(encoded_symbol[0])
 
     def generate_rules(self, buffer):
         for rule in buffer:
             head, tail = rule.split(' > ')
 
             tail = tail.split('] [')
-            generated_symbols = [extractSymbol(x) for x in tail]
+            generated_symbols = tuple([extract_symbol(x) for x in tail])
 
-            new_rule = Rule(extractSymbol(head), generated_symbols)
-            self.rules.append(new_rule)
+            new_rule = Rule(extract_symbol(head), generated_symbols)
+            self.rules.add(new_rule)
 
 
-def extractSymbol(encoded_symbol):
+def extract_symbol(encoded_symbol):
     return ''.join([c for c in encoded_symbol if c not in ' []']) # pick every char except if char is ' ' or '[' or ']'
     # join function is used to join the characters form the generated list to a single string (list of char -> String)
 
 
-def cleanLine(string, stop_char):
+def clean_line(string, stop_char):
     pos = len(string)-1
     cut_pos = 0
     stop_char_found = False
