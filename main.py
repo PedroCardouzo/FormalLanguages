76c1080bebfc371d67fc819d08150828059a15a8
Alexandre R. Saccol <arsaccol@gmail.com>
2018-05-13 21:34:26 -0300

    Add option to use shell input for easier testing. Option is completely optional :D

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import sys
 from src.Grammar import *
+import sys
 
 
 def main():
 
-    # filename handling stuff
-    file_folder = './grammars/'  # folder is test_grammars
-    filename = input('name of grammar file inside grammars (default = test.txt): ')
-    if filename == '':
-        filename = 'test.txt'
+    # Shell filename input is more
+    # convenient in enabling one to
+    # autocomplete as well as repeat
+    # previous inputs.
+    # It also enables usage of
+    # shell scripts for test automation.
+    # Leaving it as an option.
+    if len(sys.argv) == 2:
+        # If shell input is used,
+        # it is assumed that the folder is 
+        # included in input.
+        file_folder = ''
+        filename = sys.argv[1]
+
+    else: 
+        # filename handling stuff
+        file_folder = './grammars/'  # folder is test_grammars
+        filename = input('name of grammar file inside grammars (default = test.txt): ')
+        if filename == '':
+            filename = 'test.txt'
 
     grammar = Grammar()
     try:
@@ -20,7 +36,8 @@ def main():
     print('Grammar before minimization:')
     print(grammar)
 
-    grammar.minimize()
+    #grammar.minimize()
+    grammar.remove_useless_symbols()
     print('Grammar after minimization')
     print(grammar)
 
